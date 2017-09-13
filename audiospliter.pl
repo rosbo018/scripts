e2acd59da51806c9ba60698a4d7214f9cb662c0d
rosbo018 <rosbo018@uottawa.ca>
2017-09-13 19:18:24 -0400

    Add files via upload

--- audiospliter.pl
+++ audiospliter.pl
@@ -7,6 +7,9 @@
 #or
 #60-70
 #100-200
+#
+#if there is a third value, then it will become the file's name
+#ex. 60-70-aaa will create a file 60-70 sec, with name aaa
 #individual files are delimited by new line
 #times for file are delimited by - or ' '
 
@@ -23,7 +26,13 @@ $n = 1;
 foreach $line (@fileLineArr) {
 	@times = split(/\s|-/, $line);
 	$ind = index($audiofile, ".");
-	$filename = substr ( $audiofile, 0,$ind ) . "." . $n . substr ( $audiofile, $ind);
+	if ( (scalar @times) > 2){
+		$name = @times[2];
+	}
+	else {
+		$name = substr ($audiofile, 0, $ind);
+	}
+	$filename =  $name. "." . $n . substr ( $audiofile, $ind);
 	system("ffmpeg","-i",  $audiofile, "-ss", @times[0], "-to", @times[1], "-c", "copy", $filename);
 	$n= $n + 1;
 }
